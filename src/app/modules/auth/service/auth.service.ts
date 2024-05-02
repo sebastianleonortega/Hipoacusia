6440294c3bee16a2bbd05f6ffd69dedae0bbd8b4
Sebastian Leon <juanleon@wposs.com>
2024-05-02 16:19:15 -0500

    secure ls

--- src/app/modules/auth/service/auth.service.ts
+++ src/app/modules/auth/service/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  api: string = 'http://localhost:8009/';
+
+  constructor(private _http: HttpClient) { }
+
+  public register(data: any): Observable<any>{
+    return this._http.post(this.api + 'user/', data);
+  }
+
+  public login(data: any): Observable<any>{
+    return this._http.post(this.api + 'login', data);
+  }
 }
